92eb327592a6b4d249be81c163bd964fb9db2c8f
Hyugenk <kojroewhyuga@gmail.com>
2023-11-28 20:26:45 +0700

    fix : resolve issue open page when build

--- webpack.common.js
+++ webpack.common.js
@@ -92,6 +92,9 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
   ],
 };
